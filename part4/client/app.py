d86ab340da5f7d71f044a28158055b0a25f04c48
Venghour-holberton-git <13067@holbertonstudents.com>
2026-07-27 22:26:36 +1000

    restructure folder and modify index and login page

--- part4/client/app.py
+++ part4/client/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    places = requests.get("http://127.0.0.1:5000/api/v1/places/").json()
+    print(f"here they are {places}")
+    return render_template('index.html', places=places)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
