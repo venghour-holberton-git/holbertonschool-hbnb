54fdc343f248ee3794d6596814708126726b3bef
Venghour-holberton-git <13067@holbertonstudents.com>
2026-07-28 14:53:18 +1000

    filter place by price and set place details page

--- part4/client/static/scripts.js
+++ part4/client/static/scripts.js
@@ -6,6 +6,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   checkAuthentication()
   const form = document.getElementById('login-form')
+
+  document.getElementById('price-filter').addEventListener('change', async (event) => {
+    event.preventDefault()
+    let selected_value = event.target.value
+    places = await fetchPlaces()
+    console.log(places);
+    if (selected_value !== "All") {
+        places = places.filter((place) => Number(place.price) <= selected_value)
+    }
+    displayPlaces(places)
+  });
   console.log(form)
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -22,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
           method: 'POST',
           headers: {
-              'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
           },
           body: JSON.stringify({ email, password })
       });
@@ -58,7 +69,61 @@ document.addEventListener('DOMContentLoaded', () => {
     return undefined
   }
   async function fetchPlaces(token) {
+    const response = await fetch('http://127.0.0.1:5000/api/v1/places/',
+        {
+            method:'GET',
+            headers: {
+                'Authorization': `token ${getCookie('token')}`,
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    return await response.json();
       // Make a GET request to fetch places data
       // Include the token in the Authorization header
       // Handle the response and pass the data to displayPlaces function
+  }
+  function displayPlaces(places) {
+    const places_container = document.getElementById('places-list')
+    let html_child = ""
+    places.forEach(element => {
+        html_child += `
+                <h6> ${element.title }</h6>
+                <p>Price per Night ${ element.price }</p>
+                <button class="details-button">View Details</button>
+            `
+    });
+    // Might need to add description later
+    display_html = `<div class="place-card"> ${html_child} <div>`
+    places_container.innerHTML = display_html
+      // Clear the current content of the places list
+      // Iterate over the places data
+      // For each place, create a div element and set its content
+      // Append the created element to the places list
+  }
+  function getPlaceIdFromURL() {
+      // Extract the place ID from window.location.search
+      // Your code here
+  }
+  function checkPlaceAuthentication() {
+      const token = getCookie('token');
+      const addReviewSection = document.getElementById('add-review');
+
+      if (!token) {
+          addReviewSection.style.display = 'none';
+      } else {
+          addReviewSection.style.display = 'block';
+          // Store the token for later use
+          fetchPlaceDetails(token, placeId);
+      }
+  }
+  async function fetchPlaceDetails(token, placeId) {
+      // Make a GET request to fetch place details
+      // Include the token in the Authorization header
+      // Handle the response and pass the data to displayPlaceDetails function
+  }
+  function displayPlaceDetails(place) {
+      // Clear the current content of the place details section
+      // Create elements to display the place details (name, description, price, amenities and reviews)
+      // Append the created elements to the place details section
   }
