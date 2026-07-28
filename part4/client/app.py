9c3f5eb5adb5eeff549686889d7f1f045358a450
Venghour-holberton-git <13067@holbertonstudents.com>
2026-07-28 17:45:15 +1000

    place detail page and modify place_id route in the api

--- part4/client/app.py
+++ part4/client/app.py
@@ -13,9 +13,21 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('/place')
-def place():
-    return render_template('place.html')
+# @app.route('/place')
+# def place():
+#     return render_template('place.html')
+
+@app.route('/places/<place_id>')
+def place_id(place_id):
+    token = request.cookies.get("token")
+    response = requests.get(f'http://127.0.0.1:5000/api/v1/places/{place_id}', 
+        headers={
+            "Authorization": f"Bearer {token}"
+        }
+    )
+    data = response.json()
+    print(data)
+    return render_template('place.html', place_id=place_id, place_data=data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- part4/client/static/scripts.js
+++ part4/client/static/scripts.js
@@ -6,124 +6,156 @@
 document.addEventListener('DOMContentLoaded', () => {
   checkAuthentication()
   const form = document.getElementById('login-form')
-
-  document.getElementById('price-filter').addEventListener('change', async (event) => {
-    event.preventDefault()
-    let selected_value = event.target.value
-    places = await fetchPlaces()
-    console.log(places);
-    if (selected_value !== "All") {
-        places = places.filter((place) => Number(place.price) <= selected_value)
+    try {
+        document.getElementById('price-filter').addEventListener('change', async (event) => {
+        event.preventDefault()
+        let selected_value = event.target.value
+        places = await fetchPlaces()
+        console.log(places);
+        if (selected_value !== "All") {
+            places = places.filter((place) => Number(place.price) <= selected_value)
+        }
+        displayPlaces(places)
+        });
+    } catch (err) {
+        console.log("price filter is unavailable", err);
+        
     }
-    displayPlaces(places)
-  });
-  console.log(form)
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    formData = new FormData(form)
-    data = Object.fromEntries(formData)
-    console.log(data);
-    loginUser(data.email, data.password)
-  })
+//   console.log(form)
+    try {
+        form.addEventListener("submit", async (e) => {
+          e.preventDefault();
+          formData = new FormData(form)
+          
+          data = Object.fromEntries(formData)
+          console.log(data);
+          loginUser(data.email, data.password)
+        })
+    } catch (err) {
+        console.log("error ", err);
+    }
 });
 
-  async function loginUser(email, password) {
-      console.log(JSON.stringify({ email, password }));
-      
-      const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ email, password })
-      });
-      if (response.ok) {
-          const data = await response.json();
-          document.cookie = `token=${data.access_token}; path=/`;
-          window.location.href = '/';
-      } else {
-          alert('Login failed: ' + response.statusText);
-      }
-
-  }
-
-  function checkAuthentication() {
-      const token = getCookie('token');
-      const loginLink = document.getElementById('login-link');
-      console.log(token);
-      
-      if (!token) {
-          loginLink.style.display = 'block';
-      } else {
-          loginLink.style.display = 'none';
-          // Fetch places data if the user is authenticated
-          fetchPlaces(token);
-      }
-  }
-  function getCookie(name) {
-    let cookies = document.cookie.split(";")
-    console.log(cookies);
-    for (let item of cookies) {
-      if (item.startsWith(name+"=")) return item.substring(name.length + 1)
-    }
-    return undefined
-  }
-  async function fetchPlaces(token) {
-    const response = await fetch('http://127.0.0.1:5000/api/v1/places/',
-        {
-            method:'GET',
-            headers: {
-                'Authorization': `token ${getCookie('token')}`,
-                'Content-Type': 'application/json'
-            }
-        }
-    )
-    return await response.json();
-      // Make a GET request to fetch places data
-      // Include the token in the Authorization header
-      // Handle the response and pass the data to displayPlaces function
-  }
-  function displayPlaces(places) {
-    const places_container = document.getElementById('places-list')
-    let html_child = ""
-    places.forEach(element => {
-        html_child += `
-                <h6> ${element.title }</h6>
-                <p>Price per Night ${ element.price }</p>
-                <button class="details-button">View Details</button>
-            `
+async function loginUser(email, password) {
+    console.log(JSON.stringify({ email, password }));
+    
+    const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
     });
-    // Might need to add description later
-    display_html = `<div class="place-card"> ${html_child} <div>`
-    places_container.innerHTML = display_html
-      // Clear the current content of the places list
-      // Iterate over the places data
-      // For each place, create a div element and set its content
-      // Append the created element to the places list
-  }
-  function getPlaceIdFromURL() {
-      // Extract the place ID from window.location.search
-      // Your code here
-  }
-  function checkPlaceAuthentication() {
-      const token = getCookie('token');
-      const addReviewSection = document.getElementById('add-review');
+    if (response.ok) {
+        const data = await response.json();
+        document.cookie = `token=${data.access_token}; path=/`;
+        window.location.href = '/';
+    } else {
+        alert('Login failed: ' + response.statusText);
+    }
 
-      if (!token) {
-          addReviewSection.style.display = 'none';
-      } else {
-          addReviewSection.style.display = 'block';
-          // Store the token for later use
-          fetchPlaceDetails(token, placeId);
-      }
-  }
-  async function fetchPlaceDetails(token, placeId) {
-      // Make a GET request to fetch place details
-      // Include the token in the Authorization header
-      // Handle the response and pass the data to displayPlaceDetails function
-  }
-  function displayPlaceDetails(place) {
-      // Clear the current content of the place details section
-      // Create elements to display the place details (name, description, price, amenities and reviews)
-      // Append the created elements to the place details section
-  }
+}
+
+function checkAuthentication() {
+    let token = getCookie('token');
+    const loginLink = document.getElementById('login-link');
+    console.log(token);
+    
+    if (!token && !checkIsTokenExpired(token)) {
+        loginLink.style.display = 'block';
+    } else {
+        loginLink.style.display = 'none';
+        // Fetch places data if the user is authenticated
+        fetchPlaces(token);
+    }
+}
+function getCookie(name) {
+let cookies = document.cookie.split(";")
+console.log(cookies);
+for (let item of cookies) {
+    if (item.startsWith(name+"=")) return item.substring(name.length + 1)
+}
+return undefined
+}
+async function fetchPlaces(token) {
+const response = await fetch('http://127.0.0.1:5000/api/v1/places/',
+    {
+        method:'GET',
+        headers: {
+            'Authorization': `token ${getCookie('token')}`,
+            'Content-Type': 'application/json'
+        }
+    }
+)
+return await response.json();
+    // Make a GET request to fetch places data
+    // Include the token in the Authorization header
+    // Handle the response and pass the data to displayPlaces function
+}
+function displayPlaces(places) {
+const places_container = document.getElementById('places-list')
+let html_child = ""
+places.forEach(element => {
+    html_child += `
+            <h6> ${element.title }</h6>
+            <p>Price per Night ${ element.price }</p>
+            <button class="details-button">
+                <a href='http://127.0.0.1:5001/places/${element.id}'>View Details</a>
+            </button>
+        `
+});
+// Might need to add description later
+display_html = `<div class="place-card"> ${html_child} <div>`
+places_container.innerHTML = display_html
+// Clear the current content of the places list
+// Iterate over the places data
+// For each place, create a div element and set its content
+// Append the created element to the places list
+}
+function getPlaceIdFromURL() {
+// Extract the place ID from window.location.search
+// Your code here
+return window.location.search
+
+}
+function checkPlaceAuthentication() {
+    const token = getCookie('token');
+    const addReviewSection = document.getElementById('add-review');
+
+    if (!token) {
+        addReviewSection.style.display = 'none';
+    } else {
+        addReviewSection.style.display = 'block';
+        // Store the token for later use
+        fetchPlaceDetails(token, placeId);
+    }
+}
+async function fetchPlaceDetails(token, placeId) {
+    // Make a GET request to fetch place details
+    // Include the token in the Authorization header
+    // Handle the response and pass the data to displayPlaceDetails function
+    const response = fetch(`http://127.0.0.1:5000/api/v1/places/${getPlaceIdFromURL()}`, {
+    method: 'GET',
+    headers: {
+        'authorization': `token ${getCookie('token')}`,
+        'Content-Type': 'application/json'
+    },
+    })
+}
+function displayPlaceDetails(place) {
+    // Clear the current content of the place details section
+    // Create elements to display the place details (name, description, price, amenities and reviews)
+    // Append the created elements to the place details section
+}
+const checkIsTokenExpired = (token) => {
+if (!token) {
+    return true;
+}
+
+const payload = JSON.parse(atob(token.split('.')[1]));
+
+const expiry = payload.exp * 1000; // convert seconds to milliseconds
+const now = Date.now();
+
+return now >= expiry;
+}
